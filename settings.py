dd3f7febe8248d03e53a53d45b94f217b4d76ff7
Adriano Marques <adriano@rubysco.com>
2011-05-26 19:19:46 -0300

    Removed the django.contrib.flatpages.middleware.FlatpageFallbackMiddleware from settings.py

--- settings.py
+++ settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    #'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'urlrouter.middleware.URLRouterFallbackMiddleware',
 )
 
