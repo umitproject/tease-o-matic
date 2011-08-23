394794895a105368832606d70df0aded8ac9e675
Adriano Marques <py.adriano@gmail.com>
2011-08-23 18:11:59 -0300

    Added copyright headers and wrote code that allows for unsubscribing. Also added code to send emails.

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,5 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##
+## Author: Adriano Monteiro Marques <adriano@umitproject.org>
+##
+## Copyright (C) 2011 S2S Network Consultoria e Tecnologia da Informacao LTDA
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation, either version 3 of the
+## License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+##
+## You should have received a copy of the GNU Affero General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 from subscriptions.models import Subscription
-from subscriptions.forms import SubscribeForm
+from subscriptions.forms import SubscribeForm, UnsubscribeForm
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -32,6 +53,7 @@ def add(request):
             else:
 
                 sub = Subscription(email=email)
+                sub.originating_ip = request.META['REMOTE_ADDR']
                 sub.save()
 
                 # Always return an HttpResponseRedirect after successfully dealing
@@ -45,3 +67,25 @@ def add(request):
     return render_to_response('subscriptions/index.html', {
         'form': form,
     })
+
+def remove(request):
+    if request.method == 'GET':
+        form = UnsubscribeForm(request.GET)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            id = form.cleaned_data['id']
+
+            # search if email is registered
+            subscription = Subscription.objects.filter(email=email, pk=id)
+            if subscription:
+                # This means user is registered. Let's unsubscribe him
+                sub = subscription[0]
+                sub.subscribed = False
+                sub.save()
+                
+                return render_to_response('subscriptions/unsubscribed.html')
+            else:
+                # Doesn't exist... someone is trying to cheat on us. Let's pretend all is good in zion
+                return render_to_response('subscriptions/unsubscribed.html')
+    
+    return index(request)
--- urls.py
+++ urls.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##
+## Author: Adriano Monteiro Marques <adriano@umitproject.org>
+##
+## Copyright (C) 2011 S2S Network Consultoria e Tecnologia da Informacao LTDA
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation, either version 3 of the
+## License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+##
+## You should have received a copy of the GNU Affero General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
 from django.conf.urls.defaults import *
 
 handler500 = 'djangotoolbox.errorviews.server_error'
@@ -8,9 +29,7 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    (r'', include('subscriptions.urls')),
+    (r'^subscription/', include('subscriptions.urls')),
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     (r'^admin/', include(admin.site.urls)),
-    #(r'^page/$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-    #('^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
 )
